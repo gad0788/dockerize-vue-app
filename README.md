6e3cb5560ca1c2673d30845fdae45a75dfdd5bb5
gad <nday.gady@gmail.com>
2025-09-04 20:06:27 -0700

    application code repo

---
+++ README.md
@@ -0,0 +1,24 @@
+# dockerize-vue-app
+This is a demo project for dockerizing a Vue.js application.
+
+<img src="screenshots/vue-app.png" alt="vue app" />
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Welcome msg="Welcome to Vue.js App"/>
+</template>
+
+<script>
+import Welcome from './components/Welcome.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Welcome
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
